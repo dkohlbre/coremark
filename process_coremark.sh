d43890a29647d6ec4d2d81b031e8ee10cdafb3b5
dkohlbre <dkohlbre@cs.ucsd.edu>
2019-04-11 10:19:46 -0700

    Basic processing script

--- process_coremark.sh
+++ process_coremark.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo "TODO process coremark"
+TEST_LOG_FILE=${TEST_LOG_DIR}/BASE_coremark.log
+KTEST_LOG_FILE=${TEST_LOG_DIR}/KEYSTONE_coremark.log
+
+rm -f ${TEST_LOG_FILE}
+rm -f ${KTEST_LOG_FILE}
+
+for RUN_N in $(seq $REPS); do
+
+    BASE_LOG_FILE=${TEST_LOG_DIR}/base_coremark_${RUN_N}.log
+    KEYSTONE_LOG_FILE=${TEST_LOG_DIR}/keystone_coremark_${RUN_N}.log
+
+    cat ${BASE_LOG_FILE} | grep "Iterations/Sec" | cut -d':' -f '2' >> ${TEST_LOG_FILE}
+    cat ${KEYSTONE_LOG_FILE} | grep "Iterations/Sec" | cut -d':' -f '2' >> ${KTEST_LOG_FILE}
+
+done;
