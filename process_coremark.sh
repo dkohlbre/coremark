4a43bb3ca3fc4094c58449519affe1a84e06385d
dkohlbre <dkohlbre@cs.ucsd.edu>
2019-04-15 16:47:45 -0700

    Further refinements to scripts

--- process_coremark.sh
+++ process_coremark.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+source $TEST_CONFIG
 TEST_LOG_FILE=${TEST_LOG_DIR}/BASE_coremark.log
 KTEST_LOG_FILE=${TEST_LOG_DIR}/KEYSTONE_coremark.log
 
@@ -10,7 +12,10 @@ for RUN_N in $(seq $REPS); do
     BASE_LOG_FILE=${TEST_LOG_DIR}/base_coremark_${RUN_N}.log
     KEYSTONE_LOG_FILE=${TEST_LOG_DIR}/keystone_coremark_${RUN_N}.log
 
-    cat ${BASE_LOG_FILE} | grep "Iterations/Sec" | cut -d':' -f '2' >> ${TEST_LOG_FILE}
-    cat ${KEYSTONE_LOG_FILE} | grep "Iterations/Sec" | cut -d':' -f '2' >> ${KTEST_LOG_FILE}
-
+    if [[ $RUN_BASELINE == 1 ]]; then
+        cat ${BASE_LOG_FILE} | grep "Iterations/Sec" | cut -d':' -f '2' >> ${TEST_LOG_FILE}
+    fi
+    if [[ $RUN_KEYSTONE == 1 ]]; then
+        cat ${KEYSTONE_LOG_FILE} | grep "Iterations/Sec" | cut -d':' -f '2' >> ${KTEST_LOG_FILE}
+    fi
 done;
--- run_coremark.sh
+++ run_coremark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Output logs
-
+source $TEST_CONFIG
 set -e
 
 for RUN_N in $(seq $REPS); do
@@ -9,7 +9,10 @@ for RUN_N in $(seq $REPS); do
     BASE_LOG_FILE=${TEST_LOG_DIR}/base_coremark_${RUN_N}.log
     KEYSTONE_LOG_FILE=${TEST_LOG_DIR}/keystone_coremark_${RUN_N}.log
 
-    ./coremark.exe > ${BASE_LOG_FILE} 2> ${BASE_LOG_FILE}.err
-
-    ${TEST_RUNNER} coremark.exe ${EYRIE_FULL_SUPPORT} 2048 49152 0 0 > ${KEYSTONE_LOG_FILE} 2> ${KEYSTONE_LOG_FILE}.err
+    if [[ $RUN_BASELINE == 1 ]]; then
+        ./coremark.exe > ${BASE_LOG_FILE} 2> ${BASE_LOG_FILE}.err
+    fi
+    if [[ $RUN_KEYSTONE == 1 ]]; then
+        ${TEST_RUNNER} coremark.exe ${EYRIE_FULL_SUPPORT} 2048 49152 0 0 > ${KEYSTONE_LOG_FILE} 2> ${KEYSTONE_LOG_FILE}.err
+    fi
 done
